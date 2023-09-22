5a9f29f66b7a95f85d2fe6e52fdece8ed577dfe5
X1atle <myron.ryzleitsev@gmail.com>
2023-09-22 21:17:22 +0200

    some changes

--- ThirdLab/script.js
+++ ThirdLab/script.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var myBtn = document.getElementById("myBtn");
-    myBtn.addEventListener("click", function () {
-        setTimeout(function () {
-            window.location.href = "redirect.html";
-        }, 3000);
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
     var cat = document.getElementById("cat");
@@ -32,4 +24,41 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     moveCat();
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var myBtn = document.getElementById("myBtn");
+    var loginInput = document.getElementById("login");
+    var password1Input = document.getElementById("password1");
+    var password2Input = document.getElementById("password2");
+    var errorMessage = document.getElementById("error-message"); // Получаем элемент для вывода ошибок
+
+    myBtn.addEventListener("click", function (event) {
+        event.preventDefault(); // Отменяем стандартное действие отправки формы
+
+        // Проверка логина на длину и на наличие нескольких слов
+        var loginValue = loginInput.value.trim();
+        if (loginValue.length < 4 || loginValue.split(" ").length > 1) {
+            errorMessage.innerText = "Ошибка в поле 'Логин'";
+            return; // Прерываем выполнение функции, если есть ошибка
+        }
+
+        // Проверка паролей на совпадение
+        var password1Value = password1Input.value;
+        var password2Value = password2Input.value;
+        if (password1Value !== password2Value) {
+            errorMessage.innerText = "Пароли не совпадают";
+            return;
+        }
+
+        // Проверка длины паролей
+        if (password1Value.length < 4 || password2Value.length < 4) {
+            errorMessage.innerText = "Пароль слишком короткий";
+            return;
+        }
+
+        // Если все проверки пройдены, перенаправляем пользователя
+        setTimeout(function () {
+            window.location.href = "redirect.html";
+        }, 3000);
+    });
 });
